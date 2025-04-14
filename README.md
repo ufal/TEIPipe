b1e31bc648f92075dacb420d7695f3d08cde60f1
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2025-04-14 14:58:08 +0200

    add Kwalitee test

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 [![CI](https://github.com/ufal/TEIPipe/actions/workflows/test.yml/badge.svg)](https://github.com/ufal/TEIPipe/actions/workflows/test.yml)
 [![CPAN version](https://badge.fury.io/pl/TEIPipe.svg)](https://metacpan.org/pod/TEIPipe)
 [![MetaCPAN](https://img.shields.io/badge/MetaCPAN-TEIPipe-blue)](https://metacpan.org/pod/TEIPipe)
+[![CPAN Testers](https://badges.cpantesters.org/TEIPipe.svg)](https://cpantesters.org/distro/T/TEIPipe.html)
+![CPAN Upload](https://img.shields.io/cpan/last-release-date/TEIPipe)
 [![GitHub issues](https://img.shields.io/github/issues/ufal/TEIPipe.svg)](https://github.com/ufal/TEIPipe/issues)
 
 # TEIPipe
--- lib/TEIPipe.pm
+++ lib/TEIPipe.pm
@@ -1,6 +1,38 @@
 package TEIPipe;
 
-# ABSTRACT: TEIPipe base class
+# ABSTRACT: TEIPipe - a pipeline for linguistic processig TEI files
+
+
+=encoding utf8
+=head1 NAME
+
+TEIPipe - a pipeline for linguistic processig TEI files
+
+=head1 SYNOPSIS
+
+    use TEIPipe;
+    my $pipe = TEIPipe->new();
+    $pipe->run();
+
+=head1 DESCRIPTION
+
+This module concists of tools for linguistic annotations of TEI files.
+Currently, they uses LINDAT services for the annotations, but the set
+of annotations and tools is easily extensible.
+
+=head1 AUTHOR
+
+Matyáš Kopp <kopp@ufal.mff.cuni.cz>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+See L<https://dev.perl.org/licenses/>.
+
+=cut
+
 
 use strict;
 use warnings;
